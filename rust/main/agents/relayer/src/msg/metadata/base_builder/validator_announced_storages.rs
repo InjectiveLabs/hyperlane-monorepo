25ad6a21124b21b1c0a755f1d35f12ca76ce3de6
Dusan Brajovic <dbrajovic3@gmail.com>
2025-07-29 19:29:38 +0200

    fix: query validator announcement directly from chain

--- rust/main/agents/relayer/src/msg/metadata/base_builder/validator_announced_storages.rs
+++ rust/main/agents/relayer/src/msg/metadata/base_builder/validator_announced_storages.rs
@@ -1,5 +1,5 @@
 use eyre::Context;
-
+use tracing::info;
 use hyperlane_base::cache::FunctionCallCache;
 use hyperlane_core::{ValidatorAnnounce, H256};
 
@@ -13,56 +13,57 @@ pub async fn fetch_storage_locations_helper(
 ) -> eyre::Result<Vec<Vec<String>>> {
     const CTX: &str = "When fetching storage locations";
 
-    let origin = validator_announce.domain().name(); // Dynamically fetch domain name
-
-    let mut storage_locations = Vec::new();
-    let mut missing_validators = Vec::new();
-
-    for (index, validator) in validators.iter().enumerate() {
-        let key = generate_cache_key(validator);
-
-        // Attempt to retrieve from cache
-        if let Some(cached) = cache
-            .get_cached_call_result::<Vec<String>>(origin, METHOD_NAME, &key)
-            .await?
-        {
-            storage_locations.push(cached);
-        } else {
-            missing_validators.push((index, *validator));
-            storage_locations.push(Vec::new()); // Placeholder for missing validator
-        }
-    }
-
-    if missing_validators.is_empty() {
-        // Cache contains storage locations for all validators
-        return Ok(storage_locations);
-    }
-
+    // let origin = validator_announce.domain().name(); // Dynamically fetch domain name
+    //
+    // let mut storage_locations = Vec::new();
+    // let mut missing_validators = Vec::new();
+    //
+    // for (index, validator) in validators.iter().enumerate() {
+    //     let key = generate_cache_key(validator);
+    //
+    //     // Attempt to retrieve from cache
+    //     if let Some(cached) = cache
+    //         .get_cached_call_result::<Vec<String>>(origin, METHOD_NAME, &key)
+    //         .await?
+    //     {
+    //         storage_locations.push(cached);
+    //     } else {
+    //         missing_validators.push((index, *validator));
+    //         storage_locations.push(Vec::new()); // Placeholder for missing validator
+    //     }
+    // }
+    //
+    // if missing_validators.is_empty() {
+    //     // Cache contains storage locations for all validators
+    //     return Ok(storage_locations);
+    // }
+    let vals = validators.iter().map(|(v)|*v).collect::<Vec<_>>();
+    info!(?vals, "fetch_storage_locations_helper");
     // Fetch from validator_announce for missing validators
     let fetched_locations = validator_announce
         .get_announced_storage_locations(
-            &missing_validators
+            &validators
                 .iter()
-                .map(|(_, v)| *v)
+                .map(|(v)| *v)
                 .collect::<Vec<_>>(),
         )
         .await
         .context(CTX)?;
 
-    for (fetched_index, (index, validator)) in missing_validators.iter().enumerate() {
-        let key = generate_cache_key(validator);
-        let locations = &fetched_locations[fetched_index];
+    // for (fetched_index, (index, validator)) in missing_validators.iter().enumerate() {
+    //     let key = generate_cache_key(validator);
+    //     let locations = &fetched_locations[fetched_index];
+    //
+    //     // Store in cache
+    //     cache
+    //         .cache_call_result(origin, METHOD_NAME, &key, locations)
+    //         .await?;
+    //
+    //     // Update the placeholder in storage_locations
+    //     storage_locations[*index] = locations.clone();
+    // }
 
-        // Store in cache
-        cache
-            .cache_call_result(origin, METHOD_NAME, &key, locations)
-            .await?;
-
-        // Update the placeholder in storage_locations
-        storage_locations[*index] = locations.clone();
-    }
-
-    Ok(storage_locations)
+    Ok(fetched_locations)
 }
 
 /// Generates a cache key for a given validator.
